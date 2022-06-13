3a3528c26299357d24f03e3197b002b8164fc911
Lasse Benninga <devops.pipeline@example.com>
2022-06-13 23:40:46 +0200

    Cleanup

--- models/model.py
+++ models/model.py
@@ -1,16 +1,6 @@
-from pydantic import BaseModel, Extra
+from pydantic import BaseModel, Extra  # type: ignore
 from typing import Optional
 
 
 class Model(BaseModel, extra=Extra.allow):
-    # title: str
     id: Optional[str] = ""
-
-
-#     @validator("id", pre=True)
-#     def set_id(cls, v, values):
-#         return create_unique_id(values["title"])
-
-
-# def create_unique_id(title: str):
-#     return hashlib.md5(title.encode("utf-8")).hexdigest()
